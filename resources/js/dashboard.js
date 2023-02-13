701b4b1009ab2fddcac953ad6bfe8be1cc2fb12f
amirsahra <amirhosein.sahra@gmail.com>
2023-02-13 10:36:27 +0330

    feat:dashboard tmp

---
+++ resources/js/dashboard.js
@@ -0,0 +1,13 @@
+require('./bootstrap');
+
+//require('../dashboard/js/dataTables/dataTables.bootstrap.min');
+//require('../dashboard/js/dataTables/jquery.dataTables.min');
+//require('../dashboard/js/flot/')
+require('../dashboard/js/jquery.min')
+require('../dashboard/js/flot-data')
+require('../dashboard/js/bootstrap.min')
+require('../dashboard/js/metisMenu.min')
+require('../dashboard/js/morris.min')
+require('../dashboard/js/morris-data')
+//require('../dashboard/js/raphael.min')
+require('../dashboard/js/startmin')
